daad25cc9d1e378fae62e4e0f3e1769a86f4d4b3
Erodion9 <denizmavi@sabanciuniv.edu>
2020-11-22 01:35:33 +0300

    Complete chapter 03_04

---
+++ Ch03/03_04/Erodion/NaturalLanguageDemo.playground/Pages/StringTokenizer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import NaturalLanguage
+
+let text = "Knowledge will give you power, but character respect"
+
+let tagger = NLTagger(tagSchemes: [.tokenType])
+tagger.string = text
+
+tagger.enumerateTags(in: text.startIndex..<text.endIndex,
+                     unit: .word,
+                     scheme: .tokenType,
+                     options: [.omitPunctuation, .omitWhitespace]) { (tag, range) -> Bool in
+    print(text[range])
+    return true
+}
